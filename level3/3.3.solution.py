13fe05ef78c143f3ac20a7c7f65c2f5b4e392737
43h <cw.clx@outlook.com>
2023-12-13 15:32:21 +0800

    add level 3-3

---
+++ level3/3.3.solution.py
@@ -0,0 +1,15 @@
+def solution(n):
+    # Your code here
+    n = int(n)
+    i = 0
+    while n > 1:
+        if (n&1) == 0:
+            n >>= 1
+        elif (n&3) == 1 or n == 3:
+            n -= 1
+        else:
+            n += 1
+        i += 1
+    return i
+
+print(solution('15'))
